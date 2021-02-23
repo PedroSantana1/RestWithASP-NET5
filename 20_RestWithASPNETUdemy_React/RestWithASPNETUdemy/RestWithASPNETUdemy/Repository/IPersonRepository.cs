6ee42cf01f205dd9b3c3d6fa56ec3e839235bfc0
Pedro Santana <pedro.costa.santana@gmail.com>
2021-02-23 16:43:52 +0000

    Adding React

---
+++ 20_RestWithASPNETUdemy_React/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IPersonRepository : IPersonRepository<Person>
+    {
+        Person Disable(long id);
+        List<Person> FindByName(string firstName, string secondName);
+    }
+}
