30fb0bbc99a4c7f9990c4084425996a6f4eb823f
Pedro Santana <pedro.cost.santana@outlook.pt>
2021-09-27 19:48:26 +0100

    Mapping of Books Table

--- 07_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 07_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -53,6 +53,8 @@ namespace RestWithASPNETUdemy
             // Dependency Injections
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+            services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            services.AddScoped<IBookRepository, BookRepositoryImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
