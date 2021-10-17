12c1a92f905cf48d8e5f25495e1f5167725900cb
Pedro Santana <pedro.costa.santana@gmail.com>
2021-10-17 09:58:08 +0100

    Adding Docker Support

---
+++ 19_RestWithASPNETUdemy_AddingDockerSupport/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/FileBusinessImplementation.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASPNETUdemy.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class FileBusinessImplementation : IFileBusiness
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusinessImplementation(IHttpContextAccessor context)
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string filename)
+        {
+            var filePath = _basePath + filename;
+            return File.ReadAllBytes(filePath);
+        }
+
+        public async Task<FileDetailsVO> SaveFileToDisk(IFormFile file)
+        {
+            FileDetailsVO fileDetail = new FileDetailsVO();
+
+            var fileType = Path.GetExtension(file.FileName);
+            var baseUrl = _context.HttpContext.Request.Host;
+
+            if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" ||
+                fileType.ToLower() == ".png" || fileType.ToLower() == ".jpeg")
+            {
+                var docName = Path.GetFileName(file.FileName);
+                if (file != null && file.Length > 0)
+                {
+                    var destination = Path.Combine(_basePath, "", docName);
+                    fileDetail.DocumentName = docName;
+                    fileDetail.DocType = fileType;
+                    fileDetail.DocUrl = Path.Combine(baseUrl + "/api/file/v1/" + fileDetail.DocumentName);
+
+                    using var stream = new FileStream(destination, FileMode.Create);
+                    await file.CopyToAsync(stream);
+                }
+            }
+            return fileDetail;
+        }
+
+        public async Task<List<FileDetailsVO>> SaveFilesToDisk(IList<IFormFile> files)
+        {
+            List<FileDetailsVO> list = new List<FileDetailsVO>();
+            foreach (var file in files)
+            {
+                list.Add(await SaveFileToDisk(file));
+            }
+            return list;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RestWithASP-NET5
+
+[![Build Status](https://app.travis-ci.com/PedroSantana1/RestWithASP-NET5.svg?branch=main)](https://app.travis-ci.com/PedroSantana1/RestWithASP-NET5)
+
